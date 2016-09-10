55343a4912f16fdd350942fe96a8de8d12cb88ab
mdcruz <marie.descruz@gmail.com>
2016-09-10 21:49:20 +0100

    First check in

---
+++ SeleniumExample/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumExample
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private IWebDriver driver;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            driver = new FirefoxDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [TestMethod]
+        public void TestFirefox()
+        {
+            driver.Navigate().GoToUrl("http://www.google.co.uk");
+            driver.FindElement(By.Id("lst-ib")).SendKeys("selenium");
+            driver.FindElement(By.ClassName("lsb")).Click();
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            driver.Quit();
+        }
+    }
+}
